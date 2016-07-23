883cb1be4af1f5fbf07d1722c90471758a28586a
grahk93 <grace.juster@flatironschool.com>
2016-07-23 14:46:12 -0400

    Done.

--- objects.js
+++ objects.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function Dog(nameArg, breedArg, ageArg) {
+  this.name = nameArg
+  this.breed = breedArg
+  this.age = ageArg
+}
+
+function Cat(nameArg, breedArg, ageArg) {
+  this.name = nameArg
+  this.breed = breedArg
+  this.age = ageArg
+  
+}
+
+function Ferret(nameArg, ageArg, foodRequirementsArg) {
+  this.name = nameArg
+  this.age = ageArg
+  this.foodRequirements = foodRequirementsArg
+}
+
+function Bird(nameArg, breedArg) {
+  this.name = nameArg
+  this.breed = breedArg
+  
+}
+
+function Fish(nameArg, speciesArg, waterConditionsArg) {
+  this.name = nameArg
+  this.species = speciesArg
+  this.waterConditions = waterConditionsArg
+  
+}
